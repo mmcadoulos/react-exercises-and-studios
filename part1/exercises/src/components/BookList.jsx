693a4d518d07d6ad17f2b572df401376e9147298
Μιχαίας <mmcadoulos@gmail.com>
2024-03-09 21:08:05 -0600

    learned that you need class components and corresponding modules for class css

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,23 @@
-export default function BookList() {
-   let pageTitle = "Novels I Have Read";
-   let book1 = "https://www.pluggedin.com/wp-content/uploads/2020/01/scarlet-pimpernel-cover-1030x1536.jpg";
-   let book2 = "https://m.media-amazon.com/images/I/51yoPe3UyaL._AC_UF1000,1000_QL80_.jpg";
-   let book3 = "https://toppsta.com/images/covers/3/2/5/2/9780261103252.jpg";
+import React from "react";
+import classes from "./BookList.module.css";
+class BookList extends React.Component {
+  render() {
+    let pageTitle = "Novels I Have Read";
+    let book1 =
+      "https://www.pluggedin.com/wp-content/uploads/2020/01/scarlet-pimpernel-cover-1030x1536.jpg";
+    let book2 =
+      "https://m.media-amazon.com/images/I/51yoPe3UyaL._AC_UF1000,1000_QL80_.jpg";
+    let book3 = "https://toppsta.com/images/covers/3/2/5/2/9780261103252.jpg";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="The Scarlet Pimpernel by Baroness Orczy" />
-         <img src={book2} alt="The Count of Monte Cristo by Alexandre Dumas" />
-         <img src={book3} alt="The Lord of the Rings by J.R.R. Tolkien" />
-      </div>      
-   );
+    return (
+      <div className={classes.BookList}>
+        <h3>{pageTitle}</h3>
+        <img src={book1} alt="The Scarlet Pimpernel by Baroness Orczy" />
+        <img src={book2} alt="The Count of Monte Cristo by Alexandre Dumas" />
+        <img src={book3} alt="The Lord of the Rings by J.R.R. Tolkien" />
+      </div>
+    );
+  }
 }
+
+export default BookList
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,16 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from 'react';
+import classes from './ChoresList.module.css'
+
+
+class ChoresList extends React.Component {
+  render(){
+  return (
+    <div>
+      <h3 className={classes.choresHeading}>Chore List</h3>
+      <p className={classes.choresText}>Finish LaunchCode homework!</p>
+    </div>
+  );
+  }
 }
+
+export default ChoresList
